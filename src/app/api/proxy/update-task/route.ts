0ac8999f2f4daffeddb3d147d7b16e5101afdbfa
Amir Latypov <Amir.Latypov@gmail.com>
2025-01-01 12:00:25 +0300

    Edit task

--- src/app/api/proxy/update-task/route.ts
+++ src/app/api/proxy/update-task/route.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { validateUserSession } from '../user-auth';
-import { handleBaseRowRequest } from '../baserow';
+import { DB } from '@/lib/db';
+import { tasksTable } from '@/lib/schema';
+import { eq } from 'drizzle-orm';
 
 export async function POST(req: NextRequest) {
   await validateUserSession();
 
   const { id, task } = await req.json();
 
-  const result = await handleBaseRowRequest('PATCH', task, `${id}/`);
-  return NextResponse.json(result.response, { status: result.status });
+  await DB.update(tasksTable).set(task).where(eq(tasksTable.id, id));
+  const [updatedTask] = await DB.select().from(tasksTable).where(eq(tasksTable.id, id));
+
+  return NextResponse.json(updatedTask, { status: 200 });
 }
