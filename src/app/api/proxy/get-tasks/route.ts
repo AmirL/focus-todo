824d23c8a138e4ff48c8430a547907f7ca126560
Amir Latypov <Amir.Latypov@gmail.com>
2025-01-01 12:01:34 +0300

    Remove baserow

--- src/app/api/proxy/get-tasks/route.ts
+++ src/app/api/proxy/get-tasks/route.ts
@@ -1,33 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { validateUserSession } from '../user-auth';
-import { handleBaseRowRequest } from '../baserow';
 import { DB } from '@/lib/db';
 import { tasksTable } from '@/lib/schema';
-
-const STATUS_OK = 200;
 
 export async function POST(req: NextRequest) {
   await validateUserSession();
 
   const tasks = await DB.select().from(tasksTable);
-  console.log(tasks);
-  const result = await fetchAllPages();
 
-  return NextResponse.json({ ...result, tasks }, { status: result.status });
-}
-
-async function fetchAllPages() {
-  let page = 1;
-  const tasks = [];
-  let result;
-  do {
-    result = await handleBaseRowRequest('GET', null, `?page=${page}`);
-    if (result.status !== STATUS_OK) return { error: result.response, status: result.status };
-
-    tasks.push(...result.response.results);
-
-    page++;
-  } while (result.response.next > '');
-
-  return { results: tasks, status: STATUS_OK };
+  return NextResponse.json({ tasks }, { status: 200 });
 }
