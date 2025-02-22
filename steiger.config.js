8195d7f1e915ae3045dbcf069bc6679e951eaa48
Amir Latypov <Amir.Latypov@gmail.com>
2025-02-22 21:27:42 +0300

    steiger

---
+++ steiger.config.js
@@ -0,0 +1,21 @@
+// ./steiger.config.js
+import { defineConfig } from 'steiger';
+import fsd from '@feature-sliced/steiger-plugin';
+
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    files: ['./src/**'],
+    rules: {
+      'fsd/public-api': 'off',
+      'fsd/typo-in-layer-name': 'off',
+      'fsd/repetitive-naming': 'off',
+      'fsd/no-public-api-sidestep': 'off',
+      'fsd/insignificant-slice': 'off',
+    },
+  },
+  {
+    files: ['./src/*'],
+    rules: {},
+  },
+]);
