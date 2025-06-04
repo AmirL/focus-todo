64637ff6a097fda0dd377357f6e3ea075293cad8
Amir Latypov <Amir.Latypov@gmail.com>
2025-06-04 21:22:34 +0300

    Remove PWA icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 import 'reflect-metadata';
+import withPWA from 'next-pwa';
 
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  disable: isDev,
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
