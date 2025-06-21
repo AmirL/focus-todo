d3163eda326ee97f09bb5f1ac10bbf8a51cec811
Amir Latypov <Amir.Latypov@gmail.com>
2025-06-21 17:23:27 +0300

    Fix mobile viewport scaling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,9 @@ export const metadata: Metadata = {
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: 'no',
+  viewportFit: 'cover',
   shrinkToFit: 'no',
   themeColor: '#ffffff',
 };
